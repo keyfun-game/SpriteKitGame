3d84d9756ebda18dc07ae0e9fed2c19e11cca57a
keyfun-game <keyfun.game@gmail.com>
2013-11-21 13:45:35 +0800

    Updated
    Updated

---
+++ SpriteKitGame/GameOverScene.h
@@ -0,0 +1,15 @@
+//
+//  GameOverScene.h
+//  SpriteKitGame
+//
+//  Created by Key Hui on 15/11/13.
+//  Copyright (c) 2013 Key Hui. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+-(id)initWithSize:(CGSize)size won:(BOOL)won;
+
+@end
--- SpriteKitGame/ViewController.m
+++ SpriteKitGame/ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 #import "MyScene.h"
+@import AVFoundation;
+
+@interface ViewController ()
+@property (nonatomic) AVAudioPlayer * backgroundMusicPlayer;
+@end
 
 @implementation ViewController
 
@@ -15,6 +20,7 @@
 {
     [super viewDidLoad];
 
+    /*
     // Configure the view.
     SKView * skView = (SKView *)self.view;
     skView.showsFPS = YES;
@@ -26,6 +32,34 @@
     
     // Present the scene.
     [skView presentScene:scene];
+    */
+}
+
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    
+    NSError *error;
+    NSURL * backgroundMusicURL = [[NSBundle mainBundle] URLForResource:@"background-music-aac" withExtension:@"caf"];
+    self.backgroundMusicPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:backgroundMusicURL error:&error];
+    self.backgroundMusicPlayer.numberOfLoops = -1;
+    [self.backgroundMusicPlayer prepareToPlay];
+    [self.backgroundMusicPlayer play];
+    
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    if (!skView.scene)
+    {
+        skView.showsFPS = YES;
+        skView.showsNodeCount = YES;
+        
+        // Create and configure the scene.
+        SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+        scene.scaleMode = SKSceneScaleModeAspectFill;
+        
+        // Present the scene.
+        [skView presentScene:scene];
+    }
 }
 
 - (BOOL)shouldAutorotate
